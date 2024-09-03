52fb43dbf3adecfcf53916ae0d843b73197e37d1
IudySchmoller <159266787+IudySchmoller@users.noreply.github.com>
2024-09-03 10:45:39 -0300

    3/9

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 function calcula(opera) {
     let num1 = parseFloat(document.getElementById('zeroum').value);
     let num2 = parseFloat(document.getElementById('zerodois').value);
-    let reultado = 0;
+    let resultado = 0;
+    let num3 = 100;
     if(isNaN(num1)||isNaN(num2)){
         alert('acorda pra vida.');
 
     }else{
         if(opera=='+'){
-            reultado = num1 + num2;
+            resultado = num1 + num2;
         }else if(opera=='-'){
-            resultado = num1 - num2
+            resultado = num1 - num2;
         }
+        else if(opera=='*'){
+            resultado = num1 * num2;
+        }
+        else if(opera=='/'){
+            resultado = num1 / num2;
+        }else if(opera=='%'){
+            resultado = num1 % num3;
+        }    
+        document.getElementById('resultado').textContent = "resultado: " + resultado;
     }    
-    }
-
 }
