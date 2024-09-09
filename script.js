f12494f01f62a4047d431468c2244e88a2c513cb
IudySchmoller <159266787+IudySchmoller@users.noreply.github.com>
2024-09-09 09:38:17 -0300

    09/09

--- script.js
+++ script.js
@@ -1,25 +1,12 @@
-function calcula(opera) {
-    let num1 = parseFloat(document.getElementById('zeroum').value);
-    let num2 = parseFloat(document.getElementById('zerodois').value);
+function calcula() {
+    const num1 = parseFloat(document.getElementById('zeroum').value);
+    const num2 = parseFloat(document.getElementById('zerodois').value);
     let resultado = 0;
-    let num3 = 100;
-    if(isNaN(num1)||isNaN(num2)){
-        alert('acorda pra vida.');
 
-    }else{
-        if(opera=='+'){
-            resultado = num1 + num2;
-        }else if(opera=='-'){
-            resultado = num1 - num2;
-        }
-        else if(opera=='*'){
-            resultado = num1 * num2;
-        }
-        else if(opera=='/'){
-            resultado = num1 / num2;
-        }else if(opera=='%'){
-            resultado = num1 % num3;
-        }    
-        document.getElementById('resultado').textContent = "resultado: " + resultado;
-    }    
+    let oper = document.querySelector('input[name="operacao"]:checked').value;
+
+    if(oper == 'soma'){
+        resultado = num1 + num2;
+    }
+    document.getElementById('recultado').innerText = resultado;
 }
